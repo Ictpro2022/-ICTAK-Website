432ab8e8ad01d3e658a8e267a987c8da0f745434
vignesh k <74788727+vignesh-krishnan07@users.noreply.github.com>
2022-03-13 16:25:52 +0530

    first commit

---
+++ routes/contact/Contactus.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const Contact = require('../../Model/contactUsModel');
+
+// post data from form
+router.route('/add').post((req,res)=>{
+    var item = {
+        name:req.body.name,
+        email:req.body.email,
+        message:req.body.message
+    }
+    const data = new Contact(item);
+    data.save();
+    res.send("Added contact details");
+})
+
+// get data of registered students
+router.route('/get').get((req,res)=>{
+    Contact.find().then(function(contacted){
+        res.json(contacted);
+    })
+})
+
+// get data of registered students
+router.route('/count').get((req,res)=>{
+    Contact.find().then(function(contacted){
+        res.json(contacted.length);
+    })
+})
+
+module.exports=router;
